ca9ea99138d980ab9847087425d7198bf98392c0
chucky <charles.ssekitto@cobblestoneenergy.com>
2024-12-13 00:47:23 -0600

    clean up the cli app

--- download_manager/edm_cli.py
+++ download_manager/edm_cli.py
@@ -1,33 +1,10 @@
-from urllib.parse import urlparse, unquote
-import re
 import os
 import asyncio
 import argparse
-from utils.download_manager import DownloadManager
+from download_manager.core.cli_download_manager import CLIDownloadManager
+from download_manager.utils.helper_utils import extract_filename_from_url
 from logger.progress_tracker import logger
 
-download_manager = DownloadManager(max_concurrent_downloads=4)
-
-
-def sanitize_filename(filename: str) -> str:
-    """
-    Clean filename by:
-    1. Decode URL encoding (%20 etc)
-    2. Remove invalid characters
-    3. Replace spaces properly
-    4. Preserve file extension
-    """
-    # First decode URL encoded characters
-    filename = unquote(filename)
-
-    # Split filename and extension
-    name, ext = os.path.splitext(filename)
-    name = re.sub(r'[<>:"/\\|?*]', '', name)  # Remove Windows invalid chars
-    name = re.sub(r'\s+', ' ', name)          # Normalize spaces
-    name = name.strip('. ')
-    clean_filename = name + ext
-
-    return clean_filename
 
 
 def main():
@@ -47,22 +24,19 @@ def main():
     output_dir = args.output_dir or os.path.join(
         os.path.expanduser('~'), 'Downloads')
 
+    download_manager = CLIDownloadManager(max_concurrent_downloads=4)
+
     async def run_downloads():
         for url in args.urls:
-            parsed_url = urlparse(url)
-            output_file = os.path.basename(parsed_url.path)
-            output_file = sanitize_filename(output_file)
+            output_file = extract_filename_from_url(url)
+            print(f"Adding download: {url} -> {output_file}")
             if not output_file:
-                logger.error(
-                    f"Could not determine filename from URL: {
-                        url}"
-                )
+                logger.error(f"Could not determine filename from URL: {url}")
                 continue
             await download_manager.add_download(url, output_file, output_dir)
         await download_manager.run()
 
     asyncio.run(run_downloads())
-
 
 
 if __name__ == "__main__":
